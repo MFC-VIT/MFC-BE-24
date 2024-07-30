37bd1c7be2f304ab0dfba11353c0e464a360fa19
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-07-30 10:53:30 +0530

    Added Models

--- api/models/blogModel.js
+++ api/models/blogModel.js
@@ -1,3 +1,30 @@
 /**
  * { id, title, body, authorName, authoredDate }
 */
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    id:{
+        type:mongoose.Schema.ObjectId,
+        auto:true,
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    authorName:{
+        type:String,
+        required:true
+    },
+    autheredDate:{
+        tyep:Date,
+        required:true,
+        default:Date.now()
+    }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,3 +2,48 @@
  * { id, username / (firstName and lastName), emailId, password..etc }
  * (Input validation for every property (can try zod!!!))
  */
+const mongoose = require('mongoose');
+const {z} = require('zod');
+
+const userSchema = new mongoose.Schema({
+    id:{
+        type:mongoose.Schema.ObjectId,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:["Admin","user"]
+    }
+});
+const userValidationSchema = z.object({
+    username: z.string().min(3).max(30),
+    firstName: z.string().min(2).max(50),
+    lastName: z.string().min(2).max(50),
+    email: z.string().email(),
+    password: z.string().min(6),
+    role: z.string().role()
+});
+
+module.exports = mongoose.model("user",userSchema);
