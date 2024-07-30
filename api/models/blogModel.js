b80b5e318c9673eabff0ed0692ff85bad31800fb
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-07-30 21:49:32 +0530

    add hashing

--- api/models/blogModel.js
+++ api/models/blogModel.js
@@ -1,29 +1,29 @@
-/**
- * { id, title, body, authorName, authoredDate }
-*/
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-    id:{
-        type:mongoose.Schema.ObjectId,
-        auto:true,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
     },
     title:{
-        type:String,
-        required:true
+        type: String,
+        required: true,
+        trim: true,
     },
     body:{
-        type:String,
-        required:true
+        type: String,
+        required: true,
+        trim: true,
     },
     authorName:{
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
     autheredDate:{
-        tyep:Date,
-        required:true,
-        default:Date.now()
+        tyep: Date,
+        required: true,
+        default: Date.now()
     }
 });
 
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,52 +1,66 @@
-/**
- * { id, username / (firstName and lastName), emailId, password..etc }
- * (Input validation for every property (can try zod!!!))
- */
 const mongoose = require('mongoose');
-const {z} = require('zod');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    id:{
-        type:mongoose.Schema.ObjectId,
-    },
     username:{
-        type:String,
-        required:true,
-        unique:true,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
     },
     firstName: {
         type: String,
         required: true,
+        trim: true
     },
     lastName: {
         type: String,
         required: true,
+        trim: true
     },
     email:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
+        lowercase: true,
         unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+        trim: true,
+        //RFC 5322 https://emailregex.com/
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please use a valid email address.'],
     },
     password:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
+        trim: true,
     },
-    role:{
-        isAdmin:{
-            type:Boolean,
-            required:true,
-            default:false
-        }
+    isAdmin:{
+        type: Boolean,
+        required: true,
+        default: false
     }
 });
-const userValidationSchema = z.object({
-    username: z.string().min(3).max(30),
-    firstName: z.string().min(2).max(50),
-    lastName: z.string().min(2).max(50),
-    email: z.string().email(),
-    password: z.string().min(6),
-    role: z.string().role()
-});
+
+/* Hashing password for protection against potential attacks using hashing
+and Salting is done to prevent hash generate from same passwords from being same */
+userSchema.methods.createHash = async function (plainTextPassword){
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(plainTextPassword, salt);
+}
+
+/* Compares and validate password entered (candidates password) and password stored (this.password) */ 
+userSchema.methods.validatePassword = async function (candidatePassword){
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+/**
+ * @example
+ * * Signup
+    const createdUser = new Users(requestBody);
+    const hashedPassword = await createdUser.createHash(request.password); // (returns the hashed password) 
+    createdUser.password = hashedPassword;
+    await createdUser.save();
+ * * Login
+    const validatedPassword = await user.validatePassword(request.password);  // (returns boolean (true or false))
+ */
 
 module.exports = mongoose.model("user",userSchema);
