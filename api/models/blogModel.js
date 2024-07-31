670c74f9218fb6515d7d267cdc252fba3279445f
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-07-31 08:42:20 +0530

    ammended main index.js db connection and added admin controller and admin routes

--- api/models/blogModel.js
+++ api/models/blogModel.js
@@ -1,21 +1,20 @@
-/**
- * { id, title, body, authorName, authoredDate }
-*/
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
     
     title:{
-        type:String,
-        required:true
+        type: String,
+        required: true,
+        trim: true,
     },
     body:{
-        type:String,
-        required:true
+        type: String,
+        required: true,
+        trim: true,
     },
     authorName:{
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
     authoredDate:{
         type:Date,
