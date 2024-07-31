81bba2afb710abaeaa3c7019039ac76efd3e361f
ABHINAVGARG05 <abhichhrp@gmail.com>
2024-07-31 08:44:20 +0530

    added zod

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
-
+const{z} = require('zod');
 const userSchema = new mongoose.Schema({
     id:{
         type:mongoose.Schema.ObjectId,
