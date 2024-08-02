5967aa5f870caba34d4779cd8c2eba14f2e1df6a
Tanushka-lokegaonkar <tanushkaloke@gmail.com>
2024-08-02 19:24:20 +0530

    small correction made

--- api/controllers/blogcontroller.js
+++ api/controllers/blogcontroller.js
@@ -10,7 +10,7 @@ exports.getAllBlogs = async (req, res) => {
             {$skip: skip},
             { $limit: limit },
         ]);
-        res.status(201).json(blogs);
+        res.status(200).json(blogs);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
