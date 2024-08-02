16e37ae8db2f66fff9fbe2bba5785e8887fb2474
ISHAAN AGARWAL <ishaanagarwal@ISHAANs-MacBook-Air.local>
2024-08-02 19:57:07 +0530

    test2

--- api/routes/blogRoute.js
+++ api/routes/blogRoute.js
@@ -1,12 +1,12 @@
 
 const express = require('express');
-const {  createBlog, updateBlog, deleteBlog } = require('../controllers/blogController');
+const {  createBlog, updateBlog, deleteBlog,getAllBlogs } = require('../controllers/blogController');
 const { verifySession, isAdmin } = require('../middleware/authMiddleware');
 
 // const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-// router.get('/', getAllBlogs);
+router.get('/', getAllBlogs);
 router.post('/',verifySession,isAdmin, createBlog);
 router.put('/:id',verifySession,isAdmin, updateBlog);
 router.delete('/:id',verifySession,isAdmin, deleteBlog);
