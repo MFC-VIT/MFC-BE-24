b6db58d5cf64dd45c76f409877c40d327f1180b5
Harshit P G <harshitpg2004@gmail.com>
2024-08-03 18:05:01 +0530

    remove blogs

--- api/controllers/blogcontroller.js
+++ api/controllers/blogcontroller.js
@@ -1,28 +1,6 @@
 const Blog = require("../models/blogModel");
 //http://localhost:3000/api/blogs/
-exports.getAllBlogs = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) - 1 || 0;
-    const limit = parseInt(req.query.limit) || 10000000;
-    const skip = page * limit;
-    const blogs = await Blog.aggregate([
-      { $sort: { autheredDate: -1 } },
-      { $skip: skip },
-      { $limit: limit },
-    ]);
-    res.status(200).json(blogs);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-exports.test = async (req, res) => {
-  try {
-    console.log("hi");
-    res.status(200).json("done");
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+
 exports.createBlog = async (req, res) => {
   try {
     console.log(req.body);
